3f8bfad557e100feaf646d88908bbe9501ac04c4
Mahdi <mkfard@outlook.com>
2023-09-09 12:00:28 +0330

    Ditch the usages of DecoderCallback.

--- lib/src/blurhash_image.dart
+++ lib/src/blurhash_image.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:ui' as ui;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/painting.dart';
@@ -11,7 +12,9 @@ class BlurHashImage extends ImageProvider<BlurHashImage> {
   ///
   /// The arguments must not be null.
   const BlurHashImage(this.blurHash,
-      {this.decodingWidth = _DEFAULT_SIZE, this.decodingHeight = _DEFAULT_SIZE, this.scale = 1.0});
+      {this.decodingWidth = _DEFAULT_SIZE,
+      this.decodingHeight = _DEFAULT_SIZE,
+      this.scale = 1.0});
 
   /// The bytes to decode into an image.
   final String blurHash;
@@ -26,10 +29,15 @@ class BlurHashImage extends ImageProvider<BlurHashImage> {
   final int decodingHeight;
 
   @override
-  Future<BlurHashImage> obtainKey(ImageConfiguration configuration) => SynchronousFuture<BlurHashImage>(this);
+  Future<BlurHashImage> obtainKey(ImageConfiguration configuration) =>
+      SynchronousFuture<BlurHashImage>(this);
 
   @override
-  ImageStreamCompleter load(BlurHashImage key, DecoderCallback decode) => OneFrameImageStreamCompleter(_loadAsync(key));
+  ImageStreamCompleter load(
+    BlurHashImage key,
+    Future<ui.Codec> Function(Uint8List) decode,
+  ) =>
+      OneFrameImageStreamCompleter(_loadAsync(key));
 
   Future<ImageInfo> _loadAsync(BlurHashImage key) async {
     assert(key == this);
@@ -45,7 +53,9 @@ class BlurHashImage extends ImageProvider<BlurHashImage> {
   @override
   bool operator ==(Object other) => other.runtimeType != runtimeType
       ? false
-      : other is BlurHashImage && other.blurHash == blurHash && other.scale == scale;
+      : other is BlurHashImage &&
+          other.blurHash == blurHash &&
+          other.scale == scale;
 
   @override
   int get hashCode => hashValues(blurHash.hashCode, scale);
